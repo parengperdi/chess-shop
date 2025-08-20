0bff4453aa1b914e9bbb7c23758b0fba1e8bb975
Ferdi <ferdinandcoroneljr123@gmail.com>
2025-08-20 13:48:42 +0800

    changed the color palette in the product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,22 +2,26 @@ import React from "react";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="bg-green-600 p-4 rounded-xl ">
+    <div className="bg-stone-500 p-4 rounded-xl ">
       <div>
-        <img className="w-72" src="C:\Users\ferdi\Desktop\chessShop\backend\uploads\default.png" alt={product.name} />
+        <img className="w-full object-cover rounded-lg" src={`http://localhost:8080${product.imageUrl}`} alt={product.name} />
       </div>
-      
+      <div className="flex gap-4">
       <h2 className="text-lg font-semibold pl-1">{product.name}</h2>
       <p className="text-white pl-1">{product.brand}</p>
-      <p className="text-white font-bold pl-1">${product.price}</p>
-      <p className="text-sm text-white pl-1">In stock: {product.quantity}</p>
-      <p className="text-sm bg-red-400 text-white w-20 pl-1 rounded-lg mb-1">Rating: {product.rating}</p>
-      <p className="text-sm bg-yellow-400 text-white w-16 pl-1 rounded-lg">Sold: {product.numberOfSales}</p>
+      </div>
+      <div className="flex gap-4">
+        <p className="text-rose-950 font-bold pl-1">${product.price}</p>
+        <p className="text-sm text-lime-950 font-extrabold pl-1">In stock: {product.quantity}</p>
+        <p className="text-sm  text-red-950 font-extrabold w-20 pl-1 rounded-lg mb-1">Rating: {product.rating}</p>
+        <p className="text-sm text-zinc-800 font-extrabold w-20 pl-1 rounded-lg">Sold: {product.numberOfSales}</p>
+      </div>
+
       <div className="flex justify-end mt-2 gap-2">
-        <button className="bg-yellow-800 rounded-md p-2 text-white">
+        <button className="bg-stone-300 rounded-md p-2 text-black hover:bg-stone-400">
           Add to Cart
         </button>
-        <button className="bg-green-800 rounded-md p-2 text-white">
+        <button className="bg-stone-900 rounded-md p-2 text-white hover:bg-stone-950">
           Buy Now
         </button>
       </div>
