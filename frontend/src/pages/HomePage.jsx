49d3267a1212f2128334f3cbc6200f21824459f3
Ferdi <ferdinandcoroneljr123@gmail.com>
2025-08-29 10:32:57 +0800

    made the frontend function of the add to card and buy now buttons

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,11 +10,23 @@ const HomePage = () => {
     getAllProducts().then(setProducts);
   }, []);
   return (
-    <div className="max-w-7xl mx-auto px-4">
-      <h1 className="text-2xl font-bold mb-4 text-center">Welcome to Chess Shop</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        Welcome to Chess Shop
+      </h1>
+      <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard
+            key={product.id}
+            product={product}
+            mode="user"
+            onAddToCart={(product) => {
+              console.log("Add to cart", product.id);
+            }}
+            onBuyNow={(product) => {
+              console.log("Buy now", product.id);
+            }}
+          />
         ))}
       </div>
     </div>
